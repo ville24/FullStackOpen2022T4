e1b888907844165ca6dc1bd0165287d623cf6da4
ville24 <73233385+ville24@users.noreply.github.com>
2022-05-17 16:41:29 +0300

    t4.4

---
+++ utils/list_helper.js
@@ -0,0 +1,14 @@
+const logger = require('./logger')
+
+const dummy = (blogs) => {
+  return(1)
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+
+module.exports = {
+  dummy, totalLikes
+} 

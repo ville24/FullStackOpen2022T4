036155cc2d4452152fc17fbed01de0456b8b0100
ville24 <73233385+ville24@users.noreply.github.com>
2022-05-17 17:17:41 +0300

    t4.5 2

--- tests/favouriteBlog.test.js
+++ tests/favouriteBlog.test.js
@@ -1,6 +1,6 @@
 const listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
+describe('favourite blog', () => {
   const listWithOneBlog = [
     {
       _id: '5a422aa71b54a676234d17f8',
@@ -12,7 +12,7 @@ describe('total likes', () => {
     }
   ]
 
-  const blogs = [
+ blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
       title: "React patterns",
@@ -61,21 +61,40 @@ describe('total likes', () => {
       likes: 2,
       __v: 0
     }  
-  ]
+  ] 
 
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
+
+ test('when list has many blogs returns the one with most likes', () => {
+    const result = listHelper.favouriteBlog(listWithOneBlog)
+    const resultBlog = 
+      {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+    expect(result).toEqual(resultBlog)
   })
 
-  test('when list has many blogs equals the likes are calculatedt', () => {
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
+  test('when list has many blogs returns the one with most likes', () => {
+    const result = listHelper.favouriteBlog(blogs)
+    const resultBlog = 
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      }
+    expect(result).toEqual(resultBlog)
   })
 
-  test('when list is empty likes are zero', () => {
-    const result = listHelper.totalLikes([])
-    expect(result).toBe(0)
+  test('when list is empty return empty object', () => {
+    const result = listHelper.favouriteBlog([])
+    expect(result).toEqual({})
   })
 })
 

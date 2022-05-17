c25ab6df6a8474cfe35e02f0e903fcacb408c304
ville24 <73233385+ville24@users.noreply.github.com>
2022-05-17 17:14:13 +0300

    t4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  return blogs.reduce((favouriteBlog, blog) => {
+    if (favouriteBlog.likes > blog.likes)
+      return favouriteBlog
+  else
+      return blog
+  }, {})
+}
+
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favouriteBlog
 } 

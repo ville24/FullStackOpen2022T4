786997847cd37127a3ad6e4aa79e9cacc6ccd507
ville24 <73233385+ville24@users.noreply.github.com>
2023-03-04 13:27:32 +0200

    t4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
 
-  if (!blog.title || !blog.url)
+  if (!body.title || !body.url)
     next({
       name:'ValidationError',
       message: 'title or url missing'
     })
 
-  !blog.likes && (blog.likes = 0)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0,
+    user: user._id
+  })
 
-  await blog.save()
-  response.status(201).json(response.result)
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).end()
 })
 
 

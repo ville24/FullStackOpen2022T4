eb3eaf02f42133fc63ca1a42a31cc99004e50dfc
ville24 <73233385+ville24@users.noreply.github.com>
2023-03-04 14:50:41 +0200

    t4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,14 +4,6 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
 
-const getTokenFrom = request => {  
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
@@ -20,40 +12,40 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  let decodedToken = null
 
   try {
-    decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
+    const decodedToken = jwt.verify(request.token, config.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
     }
+
+    const user = await User.findById(decodedToken.id)
+
+    if (!body.title || !body.url)
+      next({
+        name:'ValidationError',
+        message: 'title or url missing'
+      })
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ? body.likes : 0,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).end()
+
   }
   catch (exception) {
     next(exception)
   }
-
-  const user = await User.findById(decodedToken.id)
-
-  if (!body.title || !body.url)
-    next({
-      name:'ValidationError',
-      message: 'title or url missing'
-    })
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes ? body.likes : 0,
-    user: user._id
-  })
-
-  const savedBlog = await blog.save()
-
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).end()
 })
 
 
